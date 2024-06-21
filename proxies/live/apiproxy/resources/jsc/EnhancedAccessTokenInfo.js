6428d7b5187b1abd07b41015f901e4c593a6b689
matthew-mbe <133137457+matthew-mbe@users.noreply.github.com>
2024-06-21 10:49:44 +0100

    more variables

--- proxies/live/apiproxy/resources/jsc/EnhancedAccessTokenInfo.js
+++ proxies/live/apiproxy/resources/jsc/EnhancedAccessTokenInfo.js
@@ -2,16 +2,20 @@ var client_id = context.getVariable('oauthv2accesstoken.GetOAuthV2Info1.client_i
 
 if (!client_id || client_id === "" || client_id === null) {
     // No products associated with the app, deny access
-    context.setVariable("enhanced-hasPostedAccessToken", "false");
-    context.setVariable("enhanced-introspectClientId", "UNKNOWN");
-    context.setVariable("enhanced-apiProductList", "UNKNOWN");
-    context.setVariable("enhanced-status", "UNKNOWN");
-    context.setVariable("enhanced-expiresIn", "UNKNOWN");
+    context.setVariable("enhanced-introspect-hasPostedAccessToken", "false");
+    context.setVariable("enhanced-introspect-clientId", "UNKNOWN");
+    context.setVariable("enhanced-introspect-apiProductList", "UNKNOWN");
+    context.setVariable("enhanced-introspect-status", "UNKNOWN");
+    context.setVariable("enhanced-introspect-expiresIn", "UNKNOWN");
+    context.setVariable("enhanced-introspect-scope", "UNKNOWN");
+    context.setVariable("enhanced-introspect-access-token", "UNKNOWN");
 }
 else {
-    context.setVariable("enhanced-hasPostedAccessToken", "true");
-    context.setVariable("enhanced-introspectClientId", client_id);
-    context.setVariable("enhanced-apiProductList", context.getVariable('oauthv2accesstoken.GetOAuthV2Info1.api_product_list'));
-    context.setVariable("enhanced-status", context.getVariable('oauthv2accesstoken.GetOAuthV2Info1.status'));
-    context.setVariable("enhanced-expiresIn", context.getVariable('oauthv2accesstoken.GetOAuthV2Info1.expires_in'));
+    context.setVariable("enhanced-introspect-hasPostedAccessToken", "true");
+    context.setVariable("enhanced-introspect-clientId", client_id);
+    context.setVariable("enhanced-introspect-apiProductList", context.getVariable('oauthv2accesstoken.GetOAuthV2Info1.api_product_list'));
+    context.setVariable("enhanced-introspect-status", context.getVariable('oauthv2accesstoken.GetOAuthV2Info1.status'));
+    context.setVariable("enhanced-introspect-expiresIn", context.getVariable('oauthv2accesstoken.GetOAuthV2Info1.expires_in'));
+    context.setVariable("enhanced-introspect-scope", context.getVariable('oauthv2accesstoken.GetOAuthV2Info1.scope'));
+    context.setVariable("enhanced-introspect-access-token", context.getVariable('oauthv2accesstoken.GetOAuthV2Info1.access_token'));
 }
