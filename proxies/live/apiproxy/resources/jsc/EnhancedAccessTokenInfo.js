3fb3de3e72031edfae07a4a702fc8361e71756f8
matthew-mbe <133137457+matthew-mbe@users.noreply.github.com>
2024-06-21 07:43:22 +0100

    extract details using javascript

---
+++ proxies/live/apiproxy/resources/jsc/EnhancedAccessTokenInfo.js
@@ -0,0 +1,11 @@
+var client_id = context.getVariable('oauthv2accesstoken.OAuthV2.GetAccessTokenInfo.client_id');
+
+if (!client_id || client_id === "" || client_id === null) {
+    // No products associated with the app, deny access
+    context.setVariable("hasPostedAccessToken", "false");
+    context.setVariable("introspectClientId", client_id);
+}
+else {
+    context.setVariable("hasPostedAccessToken", "true");
+    context.setVariable("introspectClientId", "UNKNOWN");
+}
